531941e77b092802f5cc8974ae7187f211abbf7f
oshima <oshima@rni-no-MacBook-Pro-2.local>
2016-05-13 16:11:29 +0900

    Add zsh completions https://github.com/zsh-users/zsh-completions

---
+++ .zsh/completion/ping.zsh
@@ -0,0 +1,11 @@
+_ping() {
+  _values \
+    'hosts' \
+    'mail.google.com' \
+    'www.yahoo.co.jp' \
+    '192.168.1.1' \
+    'www.google.com' \
+    'www.kantei.go.jp'
+}
+compdef _ping ping
+
